f27d59a6bcd97ee08194a3421d0e18e496d26387
Jeff <jeffrey.withington@gmail.com>
2022-01-30 21:02:39 +0000

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from flask import Flask, jsonify
-import os
+import requests
+import datetime
 
-app = Flask(__name__)
+# List of defense related words
+defenseWords = ['submarine', 'army', 'navy', 'air force', 'military', 'department of defense', 'weapon',
+'helicopter']
 
+# Time period to search
+yesterday = datetime.date.today() - datetime.timedelta(1)
+unix_time= yesterday.strftime("%s")
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+# Loop through each search term and see what's there
+for x in defenseWords:
+    r = requests.get(url=
+    f'http://hn.algolia.com/api/v1/search_by_date?tags=story&query={x}&numericFilters=created_at_i>={unix_time}')
+    data = r.json()
+    for y in data['hits']:
+        print(f"{y['title']} hit on: {x}")
